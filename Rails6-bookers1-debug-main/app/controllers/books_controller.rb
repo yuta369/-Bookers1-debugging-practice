0b6eec77fdc53b339edcdb6f4f8b61f497eb32a7
yuta369 <y.m.36912@gmail.com>
2024-05-23 16:51:13 +0000

    [Fix] 微修正

--- Rails6-bookers1-debug-main/app/controllers/books_controller.rb
+++ Rails6-bookers1-debug-main/app/controllers/books_controller.rb
@@ -8,12 +8,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
